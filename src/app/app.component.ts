95b83c52a218f62b59964ee99272c7ed7ea8aae3
anouar <anouar.bakouch@usmba.ac.ma>
2022-03-28 21:59:41 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'edocotrat_0_0_1';
+}
