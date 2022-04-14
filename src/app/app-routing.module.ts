72d9bfaa30d4b153fb7d5f17bf76c2a962668783
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-14 08:17:14 +0000

    Make root route point to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,24 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { CandidatComponent } from "./core/candidat/components/candidat.component";
-import { MainCedComponent } from "./core/directeur-ced/components/main-ced/main-ced.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CandidatComponent } from './core/candidat/components/candidat.component';
+import { MainCedComponent } from './core/directeur-ced/components/main-ced/main-ced.component';
 
+import { MainProfComponent } from './core/professeur/components/main-prof/main-prof.component';
 
-import { MainProfComponent } from "./core/professeur/components/main-prof/main-prof.component";
+import { HomeComponent } from './public/home/home.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-import { HomeComponent } from "./public/home/home.component";
-import { PageNotFoundComponent } from "./shared/page-not-found/page-not-found.component";
-
-const routes:Routes= [
-
-  
-    { path : 'professeur' , component : MainProfComponent},
-    { path : 'candidat' , component : CandidatComponent},
-    { path : 'home' , component : HomeComponent},
-    { path : 'ced' , component : MainCedComponent},
-    { path : '**' , component : PageNotFoundComponent}
-
-
+const routes: Routes = [
+  { path: 'professeur', component: MainProfComponent },
+  { path: 'candidat', component: CandidatComponent },
+  { path: 'ced', component: MainCedComponent },
+  { path: '', component: HomeComponent },
+  { path: 'home', redirectTo: '/' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
-@NgModule(
-    {
-        imports : [RouterModule.forRoot(routes)],
-        exports : [RouterModule]
-    }
-)
-
-export class AppRoutingModule{}
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
