529ff955cda142d97580b9d0ec774adfe2d43781
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-15 11:12:43 +0000

    Add constants for available groups

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -2,5 +2,22 @@ const TOKEN_KEY = 'tk';
 const REFRESH_TOKEN_KEY = 'rtk';
 const STATUS_AUTH_OK = 'status_auth_ok';
 const USER_INFO = 'uinfo';
+const CANDIDAT_GROUP_NAME = 'candidat';
+const PROFESSEUR_GROUP_NAME = 'professeur';
+const D_CED_GROUP_NAME = 'directeur_ced';
+const D_LABO_GROUP_NAME = 'directeur_labo';
+const D_POLE_GROUP_NAME = 'directeur_pole';
+const D_SCOLARITE_GROUP_NAME = 'directeur_scolarite';
 
-export { TOKEN_KEY, REFRESH_TOKEN_KEY, STATUS_AUTH_OK, USER_INFO };
+export {
+  TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+  STATUS_AUTH_OK,
+  USER_INFO,
+  CANDIDAT_GROUP_NAME,
+  PROFESSEUR_GROUP_NAME,
+  D_CED_GROUP_NAME,
+  D_LABO_GROUP_NAME,
+  D_POLE_GROUP_NAME,
+  D_SCOLARITE_GROUP_NAME,
+};
