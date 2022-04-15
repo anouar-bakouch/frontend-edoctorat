3b033134580c07fc7f68ab48ebdf3cc34e767778
anouar <anouar.bakouch@usmba.ac.ma>
2022-04-15 01:27:16 +0000

    faking sujets professors api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CandidatComponent } from './core/candidat/components/candidat.component';
 import { MainCedComponent } from './core/directeur-ced/components/main-ced/main-ced.component';
+import { DirecteurPoleComponent } from './core/directeur-pole/components/directeur-pole.component';
 import { MainProfComponent } from './core/professeur/components/main-prof/main-prof.component';
 import { HomeComponent } from './public/components/home/home.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'professeur', component: MainProfComponent },
   { path: 'candidat', component: CandidatComponent },
+  { path: 'pole', component: DirecteurPoleComponent },
   { path: 'ced', component: MainCedComponent },
   { path: '**', component: PageNotFoundComponent }
 
