1da3d8eae7b73c797181201cc7a8ed5ab0e75412
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-19 05:09:20 +0000

    Fix file upload

--- src/app/core/candidat/components/info-personnels/info-personnels.component.ts
+++ src/app/core/candidat/components/info-personnels/info-personnels.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Candidat } from 'src/app/models/Candidat';
 import { CandidatService } from '../../services/candidat.service';
 import { CountriesService } from '../../services/countries.service';
-import {RxFormBuilder, RxFormGroup} from '@rxweb/reactive-form-validators';
+import { RxFormBuilder, RxFormGroup } from '@rxweb/reactive-form-validators';
 @Component({
   selector: 'app-info-personnels',
   templateUrl: './info-personnels.component.html',
   styleUrls: ['./info-personnels.component.css'],
 })
-
 export class InfoPersonnelsComponent implements OnInit {
   private countries: any;
-  public selectedFile !:File;
-  public candidatInfoForm = <RxFormGroup> this.fservice.group({
+  public selectedFile: File | undefined;
+  public candidatInfoForm = <RxFormGroup>this.fservice.group({
     prenomCandidat: [''],
     nomCandidat: [''],
     nomCandidatAr: [''],
@@ -34,9 +33,8 @@ export class InfoPersonnelsComponent implements OnInit {
     telCandidat: [''],
     pathPhoto: [''],
     situation_familiale: [''],
-    fonctionnaire : ['']
+    fonctionnaire: [''],
   });
-
 
   public candidatInfo: Candidat | undefined;
 
@@ -47,51 +45,71 @@ export class InfoPersonnelsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
     this.httpCountries.getCountries().subscribe((res) => {
       this.countries = res.data;
     });
 
     this.getCandidatInfo();
-    
   }
 
   get _countries() {
     return this.countries;
   }
 
-  getCandidatInfo(){
-    this.candidatService.getCandidatInfo().then(res=>{
-      console.log(res)
+  getCandidatInfo() {
+    this.candidatService.getCandidatInfo().then((res) => {
+      console.log(res);
       this.candidatInfo = res;
-      this.candidatInfoForm.get('prenomCandidat')?.setValue(this.candidatInfo.prenom);
-      this.candidatInfoForm.get('prenomCandidatAr')?.setValue(this.candidatInfo.prenomCandidatAr);
+      this.candidatInfoForm
+        .get('prenomCandidat')
+        ?.setValue(this.candidatInfo.prenom);
+      this.candidatInfoForm
+        .get('prenomCandidatAr')
+        ?.setValue(this.candidatInfo.prenomCandidatAr);
       this.candidatInfoForm.get('nomCandidat')?.setValue(this.candidatInfo.nom);
-      this.candidatInfoForm.get('nomCandidatAr')?.setValue(this.candidatInfo.nomCandidatAr);
+      this.candidatInfoForm
+        .get('nomCandidatAr')
+        ?.setValue(this.candidatInfo.nomCandidatAr);
       this.candidatInfoForm.get('cinCandidat')?.setValue(this.candidatInfo.cin);
       this.candidatInfoForm.get('cneCandidat')?.setValue(this.candidatInfo.cne);
       this.candidatInfoForm.get('adresse')?.setValue(this.candidatInfo.adresse);
-      this.candidatInfoForm.get('adresseAr')?.setValue(this.candidatInfo.adresseAr);
+      this.candidatInfoForm
+        .get('adresseAr')
+        ?.setValue(this.candidatInfo.adresseAr);
       this.candidatInfoForm.get('pays')?.setValue(this.candidatInfo.pays);
       this.candidatInfoForm.get('ville')?.setValue(this.candidatInfo.ville);
-      this.candidatInfoForm.get('villeDeNaissance')?.setValue(this.candidatInfo.villeDeNaissance);
-      this.candidatInfoForm.get('villeDeNaissanceAr')?.setValue(this.candidatInfo.villeDeNaissanceAr);
-      this.candidatInfoForm.get('dateDeNaissance')?.setValue(this.candidatInfo.dateDeNaissance);
-      this.candidatInfoForm.get('typeHandicap')?.setValue(this.candidatInfo.typeDeHandiCape);
-      this.candidatInfoForm.get('academie')?.setValue(this.candidatInfo.academie);
-      this.candidatInfoForm.get('mailCandidat')?.setValue(this.candidatInfo.email);
-      this.candidatInfoForm.get('telCandidat')?.setValue(this.candidatInfo.telCandidat);
-      this.candidatInfoForm.get('pathPhoto')?.setValue(this.candidatInfo.pathPhoto);
-      this.candidatInfoForm.get('situation_familiale')?.setValue(this.candidatInfo.situation_familiale);
-      this.candidatInfoForm.get('foctionnaire')?.setValue(this.candidatInfo.fonctionnaire);
-      
-    })
+      this.candidatInfoForm
+        .get('villeDeNaissance')
+        ?.setValue(this.candidatInfo.villeDeNaissance);
+      this.candidatInfoForm
+        .get('villeDeNaissanceAr')
+        ?.setValue(this.candidatInfo.villeDeNaissanceAr);
+      this.candidatInfoForm
+        .get('dateDeNaissance')
+        ?.setValue(this.candidatInfo.dateDeNaissance);
+      this.candidatInfoForm
+        .get('typeHandicap')
+        ?.setValue(this.candidatInfo.typeDeHandiCape);
+      this.candidatInfoForm
+        .get('academie')
+        ?.setValue(this.candidatInfo.academie);
+      this.candidatInfoForm
+        .get('mailCandidat')
+        ?.setValue(this.candidatInfo.email);
+      this.candidatInfoForm
+        .get('telCandidat')
+        ?.setValue(this.candidatInfo.telCandidat);
+      this.candidatInfoForm
+        .get('situation_familiale')
+        ?.setValue(this.candidatInfo.situation_familiale);
+      this.candidatInfoForm
+        .get('foctionnaire')
+        ?.setValue(this.candidatInfo.fonctionnaire);
+    });
   }
 
-
-  updateCandidatInfo(){
-
-/*
+  updateCandidatInfo() {
+    /*
     let candidat:Candidat = {
 
       prenom : this.candidatInfoForm.get('prenomCandidat')?.value,
@@ -123,20 +141,14 @@ export class InfoPersonnelsComponent implements OnInit {
     */
 
     let formdata = this.candidatInfoForm.toFormData();
-    formdata.append('pathPhoto',this.selectedFile,this.selectedFile.name);
+    this.selectedFile &&
+      formdata.set('pathPhoto', this.selectedFile, this.selectedFile.name);
     this.candidatService.updateCandidatInfo(formdata);
-
   }
 
-  onFileSelected(event:any){
-
-   this.selectedFile = <File> event.target.files[0];
- 
-   alert('hi')
-   console.log(this.selectedFile)
-
+  public onFileSelected(event: any) {
+    if (event.target.files.length > 0) {
+      this.selectedFile = <File>event.target.files[0];
+    }
   }
-
-
-
 }
