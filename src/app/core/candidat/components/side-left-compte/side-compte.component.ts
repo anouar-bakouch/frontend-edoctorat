df388d5dbc70ff0a6bb6d04307788b7ea7d1d056
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-23 05:46:45 +0000

    Show candidat profile image

--- src/app/core/candidat/components/side-left-compte/side-compte.component.ts
+++ src/app/core/candidat/components/side-left-compte/side-compte.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { filter } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import UserInfo from 'src/app/models/UserInfo';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: '[app-side-compte]',
@@ -18,6 +19,7 @@ export class SideCompteComponent implements OnInit {
       .pipe(filter((u) => u != undefined))
       .subscribe((uinfo) => {
         this.candidatInfo = uinfo!;
+        this.candidatInfo.pathPhoto = `${environment.API_URL}${this.candidatInfo.pathPhoto}`
       });
   }
 }
