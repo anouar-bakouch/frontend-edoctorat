88938c98d3bbebe3858e96efde86b88c760ebf50
anouar <anouar.bakouch@usmba.ac.ma>
2022-04-24 12:51:36 +0000

    diplome service

--- src/app/core/candidat/services/candidat-parcours.service.ts
+++ src/app/core/candidat/services/candidat-parcours.service.ts
@@ -16,8 +16,42 @@ export class CandidatParcoursService {
 
   //bac
 
-  addDiplome(diplome:Diplome):Observable<Diplome>{
-     return this.http.post<Diplome>(environment.API_URL+'/api/candidat-parcours/',diplome)
+
+  addDiplome(diplome:Diplome):Promise<Diplome> {
+    return new Promise((resolve, reject) => {
+      this.http
+        .post<Diplome>(`${environment.API_URL}/api/candidat-parcours/`, diplome)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
   }
+
+  updateDiplome(diplome:Diplome):Promise<Diplome> {
+    return new Promise((resolve, reject) => {
+      this.http
+        .put<Diplome>(`${environment.API_URL}/api/candidat-parcours/${diplome.idDiplome}`, diplome)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
+
+   deleteDiplome(diplome:Diplome):Observable<Diplome>{
+     return this.http.delete<Diplome>(`${environment.API_URL}/api/candidat-parcours/${diplome.idDiplome}`);
+   }
+
+
+
 
 }
