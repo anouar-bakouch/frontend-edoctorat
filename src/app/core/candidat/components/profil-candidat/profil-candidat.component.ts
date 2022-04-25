9aee85498ef69abe4b5f7bfa54cb87a26954cd4f
Mohammed Akram Ibriz <92886184+akramIbriz@users.noreply.github.com>
2022-04-25 23:57:46 +0000

    profil candidat

---
+++ src/app/core/candidat/components/profil-candidat/profil-candidat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-candidat',
+  templateUrl: './profil-candidat.component.html',
+  styleUrls: ['./profil-candidat.component.css']
+})
+export class ProfilCandidatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
