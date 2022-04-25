c40d506865b70a428414753b06a5ba076599bd45
anouar <anouar.bakouch@usmba.ac.ma>
2022-04-25 23:21:17 +0000

    changing dut in enum

--- src/app/enums/DiplomeType.ts
+++ src/app/enums/DiplomeType.ts
@@ -1,10 +1,12 @@
 export enum DiplomeType {
+
   BAC = 'baccalauréat',
-  DUT = 'dut',
+  DUT = 'diplome universitaire de technologie',
   CI = 'cycle ingénieur',
   LICENCE = 'licence',
   MASTER = 'master',
   MASTER_SPECIALISE = 'master specialise',
   MASTER_EN_SCIENCE_TECHNIQUE = 'master en science et technique',
-  DOCTORATE_EN_MEDICINE = 'doctorate en medicine',
+  DOCTORATE_EN_MEDICINE = 'doctorate en medicine'
+  
 }
