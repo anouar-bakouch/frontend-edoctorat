fa273b940caf1bdf2c98ed6d3e4b1c969726b067
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-04-28 17:23:04 +0000

    mes candidats prof ts file

--- src/app/core/professeur/components/prof-resultat/prof-resultat.component.ts
+++ src/app/core/professeur/components/prof-resultat/prof-resultat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Examiner } from 'src/app/models/Examiner';
+import Result from 'src/app/models/Result';
+import { OperationsService } from '../../services/operations.service';
 
 @Component({
   selector: '[app-prof-resultat]',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prof-resultat.component.css']
 })
 export class ProfResultatComponent implements OnInit {
-
-  constructor() { }
+  public resultats: Examiner[] = [];
+  public resultat:Result<Examiner> = {
+    count: 0,
+    next: '',
+    previous: '',
+    results: []
+  }
+  constructor(private operationsService: OperationsService) { }
 
   ngOnInit(): void {
+    this.getResultats()
   }
-
+  getResultats() {
+    this.operationsService.getResultats().subscribe(data => {
+      // console.log(data)
+      this.resultat = data;
+      this.resultats = this.resultat.results
+    })
+  }
 }
