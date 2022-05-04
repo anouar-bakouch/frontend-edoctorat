b4c416d00df6cc0b74885110720f60ecbd8b865a
lokaimoma <kelvinclark2k77@gmail.com>
2022-05-04 18:20:52 +0000

    Add custom alert component

---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css'],
+})
+export class AlertComponent implements OnInit {
+  data: AlertData | undefined;
+  tcolor: string = 'black';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.data) {
+      switch (this.data.type.toLocaleLowerCase()) {
+        case 'success':
+          this.tcolor = '#4cbb17';
+          break;
+        case 'error':
+          this.tcolor = 'red';
+          break;
+        case 'loading':
+        case 'info':
+          this.tcolor = 'blue';
+          break;
+      }
+    }
+  }
+}
+
+export type AlertData = {
+  type: string | undefined;
+  message: string | undefined;
+};
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { OffsetpaginationComponent } from './components/offsetpagination/offsetpagination.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
-  declarations: [PageNotFoundComponent, OffsetpaginationComponent],
+  declarations: [
+    PageNotFoundComponent,
+    OffsetpaginationComponent,
+    AlertComponent,
+  ],
   imports: [CommonModule],
-  exports: [PageNotFoundComponent, OffsetpaginationComponent],
+  exports: [PageNotFoundComponent, OffsetpaginationComponent, AlertComponent],
 })
 export class SharedModule {}
