92fccda4e609bfb73d6c84e2ed75d1498e8ce819
anouar <anouar.bakouch@usmba.ac.ma>
2022-05-06 16:32:10 +0000

    progress bar in green after hitting 10

--- src/app/core/candidat/components/candidat-mini-header/parcours.component.ts
+++ src/app/core/candidat/components/candidat-mini-header/parcours.component.ts
@@ -3,6 +3,7 @@ import { DiplomeType } from 'src/app/enums/DiplomeType';
 import { CandidatParcoursService } from '../../services/candidat-parcours.service';
 import { CandidatService } from '../../services/candidat.service';
 import { Diplome } from 'src/app/models/Diplome';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: '[app-parcours]',
@@ -11,31 +12,91 @@ import { Diplome } from 'src/app/models/Diplome';
 })
 export class ParcoursComponent implements OnInit {
 
-  public parcours = this.candidatService.TypesDiplomes;;
+  public parcours:{
+    name: string;
+    value: DiplomeType;
+}[] = [];
+  public parcoursNotCompleted = this.parcours.filter(x=>{
+    x.value = DiplomeType.BAC;
+  })
+
+
+  verifyParcours(){
+    
+  }
 
   constructor(
     public candidatService :CandidatService,
     private candidatParcours: CandidatParcoursService) { }
 
-  public BacExist:boolean = false;
-  public DutExist:boolean = false;
-  public CIExist:boolean = false;
-  public LicenceExist:boolean = false;
-  public LicenceProExist:boolean = false;
-  public MasterExist:boolean = false;
-  public MasterSpeExist:boolean = false;
   public diplomes: Diplome [] = [];
-  public progressBarvalue:string = '0';
+  public progressBarvalue:number = 0;
+  public OtherDiplomes:boolean = false;
+
+  public bac_1 = 4;
+  public bac_2 = 0;
+  public bac_3 = 0;
+  public bac_5 = 0;
+  public bac_6 = 0;
+
+  public SuccessParcours = false;
+
+
 
 
   ngOnInit(): void {
+
+    this.parcours = this.candidatService.TypesDiplomes;
+
     this.candidatParcours.getInfoDiplomes().then(res=>{
       this.diplomes = res.results;
 
       this.diplomes.forEach(x=>{
-        if(x.intitule = DiplomeType.BAC) this.BacExist = true;
-      })
+        
+        switch(x.intitule){
 
+          case DiplomeType.BAC :
+                  this.bac_1 = this.bac_1 + 16;
+                  console.log('bac')
+          break;
+
+          case DiplomeType.DUT : 
+                   this.bac_2 = this.bac_2 + 16;
+                  console.log('dut')
+
+          break;
+
+          case DiplomeType.CI || DiplomeType.MASTER || DiplomeType.MASTER_SPECIALISE || DiplomeType.MASTER_EN_SCIENCE_TECHNIQUE : 
+                   this.bac_5 = this.bac_5 + 16;
+                  console.log('ci')
+
+          break;
+
+
+          case DiplomeType.LICENCE || DiplomeType.LICENCE_PROFESSIONNELLE : 
+                   this.bac_3 = this.bac_3 + 16;
+                  console.log('licence')
+
+          break;
+
+          case DiplomeType.DOCTORATE_EN_MEDICINE : 
+                   this.bac_6 = this.bac_6 + 16;
+          break;
+
+          default : 
+                  this.progressBarvalue = 0;
+          break;
+
+        }
+    
+
+      }
+    
+      )
+      this.progressBarvalue = this.bac_1+ this.bac_2 + this.bac_6 + this.bac_5 + this.bac_3;
+      if(this.progressBarvalue === 100){
+        this.SuccessParcours = ! this.SuccessParcours;
+      }
     })
   }
 
