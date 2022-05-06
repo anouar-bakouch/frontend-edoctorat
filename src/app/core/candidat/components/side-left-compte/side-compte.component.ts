74da393bf6032b602f166f2e9557d76e61fd8ac8
anouar <anouar.bakouch@usmba.ac.ma>
2022-05-06 17:22:49 +0000

    implementing config of postuelr

--- src/app/core/candidat/components/side-left-compte/side-compte.component.ts
+++ src/app/core/candidat/components/side-left-compte/side-compte.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { filter } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import Config from 'src/app/models/Config';
 import UserInfo from 'src/app/models/UserInfo';
 import { environment } from 'src/environments/environment';
+import swal from 'sweetalert';
 import { CandidatService } from '../../services/candidat.service';
 
 @Component({
@@ -11,11 +13,46 @@ import { CandidatService } from '../../services/candidat.service';
   styleUrls: ['./side-compte.component.css'],
 })
 export class SideCompteComponent implements OnInit {
-  public candidatInfo!: UserInfo;
 
-  constructor(public authService: AuthService,candidatService:CandidatService) {}
+  public candidatInfo!: UserInfo;
+  public config!: Config;
+  public date_debut:any;
+  public date_fin:any;
+  public actual_date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+  public disablePostuler:boolean = false;
+  public message:string = 'vous n\'avez plus le droit de postuler ';
+
+  constructor(public authService: AuthService,public candidatConfig:CandidatService) {}
+
+  getConfigInfo() {
+    this.candidatConfig.getConfigInfo().then((res) => {
+      this.config = res;
+      this.date_debut = res.date_debut_postuler_sujet_candidat;
+      this.date_fin = res.date_fin_postuler_sujet_candidat;
+
+      
+      if(this.actual_date > this.date_fin){
+        this.disablePostuler = ! this.disablePostuler;
+      }
+
+      if(this.actual_date < this.date_debut){
+        this.disablePostuler;
+      }
+
+    });
+  }
+
+  onClick(){
+    swal({
+     text : this.message
+    });
+  }
+
 
   ngOnInit(): void {
+
+    this.getConfigInfo();
+  
     this.authService.currentUserSubjet
       .pipe(filter((u) => u != undefined))
       .subscribe((uinfo) => {
