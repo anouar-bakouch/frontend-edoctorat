c0e662d09d2d3b1acd298d6bb407c809107c9fbf
anouar <anouar.bakouch@usmba.ac.ma>
2022-05-11 10:31:39 +0100

    new implementation of verfiying dossier

--- src/app/core/candidat/components/candidat-mini-header/parcours.component.ts
+++ src/app/core/candidat/components/candidat-mini-header/parcours.component.ts
@@ -29,14 +29,15 @@ export class ParcoursComponent implements OnInit {
     private candidatParcours: CandidatParcoursService) { }
 
   public diplomes: Diplome [] = [];
-  public progressBarvalue:number = 0;
   public OtherDiplomes:boolean = false;
 
-  public bac_1 = 0;
-  public bac_2 = 0;
-  public bac_3 = 0;
-  public bac_5 = 0;
-  public bac_6 = 0;
+  public bac_1:boolean = false;
+  public bac_2:boolean = false;
+  public bac_3:boolean = false;
+  public bac_5:boolean = false;
+  public bac_6:boolean = false;
+  public progressBarvalue:number = 0;
+
 
   public SuccessParcours = false;
 
@@ -52,26 +53,27 @@ export class ParcoursComponent implements OnInit {
         switch(x.intitule){
 
           case DiplomeType.BAC :
-                  this.bac_1 = this.bac_1 + 20;
+                  this.bac_1 = !this.bac_1;
+                  
           break;
 
           case DiplomeType.DUT || DiplomeType.BTS || DiplomeType.DTS || DiplomeType.DEUG || DiplomeType.DEUST: 
-                   this.bac_2 = this.bac_2 + 20;
+                   this.bac_2 = !this.bac_2;
 
           break;
 
           case DiplomeType.CI || DiplomeType.MASTER || DiplomeType.MASTER_SPECIALISE || DiplomeType.MASTER_EN_SCIENCE_TECHNIQUE : 
-                   this.bac_5 = this.bac_5 + 60;
+                   this.bac_5 = !this.bac_5;
 
           break;
 
           case DiplomeType.LICENCE || DiplomeType.LICENCE_PROFESSIONNELLE : 
-                   this.bac_3 = this.bac_3 + 20;
+                   this.bac_3 = !this.bac_3;
 
           break;
 
           case DiplomeType.DOCTORATE_EN_MEDICINE : 
-                   this.bac_6 = this.bac_6 + 80;
+                   this.bac_6 = !this.bac_6;
           break;
 
           default : 
@@ -84,10 +86,7 @@ export class ParcoursComponent implements OnInit {
       }
     
       )
- 
-
-      const result:number = this.bac_1+ this.bac_2 + this.bac_3 + this.bac_5 + this.bac_6 ;
-      if(result === 80 || result === 60 || result === 100 ||  result >= 120){
+      if(this.bac_1 && this.bac_6){
         this.SuccessParcours = ! this.SuccessParcours;
         this.progressBarvalue = 100;
         this.message = 'félicitations votre dossier est complet !';
@@ -95,12 +94,49 @@ export class ParcoursComponent implements OnInit {
           icon: 'success',
          text : this.message
         });
+      }
 
+      if(this.bac_1 && this.bac_5){
+        this.SuccessParcours = ! this.SuccessParcours;
+        this.progressBarvalue = 100;
+        this.message = 'félicitations votre dossier est complet !';
+        swal({
+          icon: 'success',
+         text : this.message
+        });
+      }
+
+      if(this.bac_1  && this.bac_2 && this.bac_3 && this.bac_5){
+        this.SuccessParcours = ! this.SuccessParcours;
+        this.progressBarvalue = 100;
+        this.message = 'félicitations votre dossier est complet !';
+        swal({
+          icon: 'success',
+         text : this.message
+        });
+      }
       
+      if(this.bac_1 && this.bac_3 && this.bac_5){
+        this.SuccessParcours = ! this.SuccessParcours;
+        this.progressBarvalue = 100;
+        this.message = 'félicitations votre dossier est complet !';
+        swal({
+          icon: 'success',
+         text : this.message
+        });
       }
-      else {
-        this.progressBarvalue = result;
+    
+      if(this.bac_1) {
+        this.progressBarvalue = 20;
       }
+      if(this.bac_1 && this.bac_2){
+        this.progressBarvalue = 40;
+      }
+      if(this.bac_1 && this.bac_2 && this.bac_3){
+        this.progressBarvalue = 70;
+      }
+
+
     })
   }
 
