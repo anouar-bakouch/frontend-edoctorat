1a4d7c2a8d89f4e1c5e137e8513632b2fdccadae
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-12 09:51:14 +0100

    add directeur labo main component

---
+++ src/app/core/directeur-labo/components/main-labo/main-labo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-labo',
+  templateUrl: './main-labo.component.html',
+  styleUrls: ['./main-labo.component.css']
+})
+export class MainLaboComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
