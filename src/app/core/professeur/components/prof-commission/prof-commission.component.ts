14fc24a7b26c9113693c13b9b25b3f36c43ce22e
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-13 11:16:55 +0100

    fixe prof commsission

--- src/app/core/professeur/components/prof-commission/prof-commission.component.ts
+++ src/app/core/professeur/components/prof-commission/prof-commission.component.ts
@@ -3,6 +3,7 @@ import { Commission } from 'src/app/models/Commission';
 import { Participe } from 'src/app/models/Participe';
 import { Professeur } from 'src/app/models/Professeur';
 import Result from 'src/app/models/Result';
+import { AlertData } from 'src/app/shared/components/alert/alert.component';
 import { OperationsService } from '../../services/operations.service';
 
 @Component({
@@ -11,6 +12,8 @@ import { OperationsService } from '../../services/operations.service';
   styleUrls: ['./prof-commission.component.css']
 })
 export class ProfCommissionComponent implements OnInit {
+  public alert: AlertData | undefined = undefined;
+  public loading: boolean = false;
   public participes: Participe[] = [];
   public commissions: Commission[] = [];
   public commission: Commission = {
@@ -40,9 +43,17 @@ export class ProfCommissionComponent implements OnInit {
   }
 
   getCommissions(){
-    this.operationsService.getCommissions().subscribe(data => {
+    this.loading = true
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+    this.operationsService.getCommissions().then(data => {
+      this.loading = false
       console.log(data)
-      this.commissions = data;
+      this.result = data as Result<Commission>;
+      this.commissions = this.result.results;
+      // this.commissions = data;
       console.log(this.commissions)
       // for (var i = 0; i < this.commissions.participants.length; i++) {
 
@@ -53,6 +64,19 @@ export class ProfCommissionComponent implements OnInit {
 
       // }
       // this.commissions.push(this.commission)
-    })
+    }).catch((error) => {
+      console.log(error)
+      this.alert = {
+        type: 'error',
+        message: 'error',
+      };
+    }).finally(() => {
+      this.loading = false
+      this.alert = {
+        type: 'success',
+        message: 'success',
+      };
+      setTimeout(() => (this.alert = undefined), 3000);
+    });
   }
 }
