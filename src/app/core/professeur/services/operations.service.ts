229e6b34ed38cd5499116d8307b844fc833a2c5a
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-13 11:18:13 +0100

    fixe prof services

--- src/app/core/professeur/services/operations.service.ts
+++ src/app/core/professeur/services/operations.service.ts
@@ -11,78 +11,187 @@ import Result from 'src/app/models/Result';
 import { Sujet } from 'src/app/models/Sujet';
 import { environment } from 'src/environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OperationsService {
 
+  constructor(private http: HttpClient) {}
 
-  // private _url: string = 'http://129.151.236.119';
-  private _url: string = 'http://127.0.0.1:8000';
-  private _url2:string = 'http://127.0.0.1:8000';
-  // private _url2: string = 'http://129.151.236.119';
-  constructor(private http: HttpClient) { }
-
-  public getFormationDoctorales(): Observable<Result<FormationDoctorale>> {
-    return this.http.get<Result<FormationDoctorale>>(environment.API_URL + '/api/formation-doctorale/');
-
-  }
-
-  public getProfesseurs(): Observable<Result<Professeur>> {
+  public getFormationDoctorales() {
+    return new Promise((resolve, reject) => {
+      this.http.get(
+        environment.API_URL + '/api/formation-doctorale/'
+      ).subscribe(
+        {
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        }
+      )
+    })
     
-    return this.http.get<Result<Professeur>>(environment.API_URL +'/api/get-professeurs/');
-
-  }
-  public getProfesseur(id: Professeur): Observable<Professeur> {
-    return this.http.get<Professeur>(environment.API_URL +`/api/get-professeurs/${id}/`);
-
-  }
-  public getformDoct(id: FormationDoctorale): Observable<FormationDoctorale> {
-    return this.http.get<FormationDoctorale>(environment.API_URL +`/api/formation-doctorale/${id}/`);
-
-  }
-  public getSujets(): Observable<Result<Sujet>> {
-
-    return this.http.get<Result<Sujet>>(environment.API_URL +'/api/sujets/');
-
-  }
-  public getSujet(id:number): Observable<Sujet> {
-
-    return this.http.get<Sujet>(environment.API_URL + '/api/sujets/' + id + '/');
-
   }
 
-  public addSujet(sujet: JSON): Observable<Sujet> {
-    return this.http.post<Sujet>(environment.API_URL + '/api/sujets/', sujet);
-
-  }
-
-  public deleteSujet(sujet: Sujet): Observable<Sujet> {
-    console.log(sujet)
-    return this.http.delete<Sujet>(environment.API_URL + '/api/sujets/' + sujet.id + '/');
-
-  }
-
-  public updateSujet(sujet: JSON, id:number): Observable<Sujet> {
-    return this.http.put<Sujet>(environment.API_URL + '/api/sujets/' + id + '/', sujet);
-
-  }
-
-
-  public getCommissions(): Observable<Commission[]> {
-
-    return this.http.get<Commission[]>(environment.API_URL + '/api/participant/');
-
-  }
-  
-  public getResultats(): Observable<Result<Examiner>> {
+  public getProfesseurs(){
+    return new Promise((resolve, reject) => {
+      this.http.get(
+        environment.API_URL + '/api/get-professeurs/'
+      ).subscribe(
+        {
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        }
+      )
+    })
     
-    return this.http.get<Result<Examiner>>(environment.API_URL + '/api/examiner/');
+  }
+  // public getProfesseur(id: Professeur): Observable<Professeur> {
+  //   return this.http.get<Professeur>(
+  //     environment.API_URL + `/api/get-professeurs/${id}/`
+  //   );
+  // }
+  // public getformDoct(id: FormationDoctorale): Observable<FormationDoctorale> {
+  //   return this.http.get<FormationDoctorale>(
+  //     environment.API_URL + `/api/formation-doctorale/${id}/`
+  //   );
+  // }
+  public getSujets(){
+    return new Promise((resolve, reject) => {
+      this.http.get(environment.API_URL + '/api/sujets/').subscribe(
+        {
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        }
+      )
+    })
+    
+  }
+  public getSujet(id: number): Observable<Sujet> {
+    return this.http.get<Sujet>(
+      environment.API_URL + '/api/sujets/' + id + '/'
+    );
+  }
 
+  // public addSujet(sujet: object): Observable<Sujet> {
+  //   return this.http.post<Sujet>(environment.API_URL + '/api/sujets/', sujet);
+  // }
+  public addSujet(sujet: object) {
+    return new Promise((resolve, reject) => {
+      this.http.post(environment.API_URL + '/api/sujets/', sujet).subscribe(
+        {
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        }
+      )
+    })
+  }
+
+  // public deleteSujet(sujet: Sujet): Observable<Sujet> {
+  //   console.log(sujet)
+  //   return this.http.delete<Sujet>(environment.API_URL + '/api/sujets/' + sujet.id + '/');
+
+  // }
+
+  public deleteSujet(sujet: Sujet) {
+    return new Promise((resolve, reject) => {
+      this.http
+        .delete(environment.API_URL + '/api/sujets/' + sujet.id + '/')
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
+  public updateSujet(sujet: object, id: number) {
+    return new Promise((resolve, reject) => {
+      this.http
+        .put(environment.API_URL + '/api/sujets/' + id + '/', sujet)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
+
+  // public getCommissions(): Observable<Result<Commission>> {
+  //   return this.http.get<Result<Commission>>(
+  //     environment.API_URL + '/api/commission/'
+  //   );
+  // }
+  public getCommissions(){
+    return new Promise((resolve, reject) => {
+      this.http.get(
+        environment.API_URL + '/api/commission/'
+      ).subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
+  // public getResultats(): Observable<Result<Examiner>> {
+  //   return this.http.get<Result<Examiner>>(
+  //     environment.API_URL + '/api/examiner/'
+  //   );
+  // }
+  public getResultats(){
+    return new Promise((resolve, reject) => {
+      this.http.get(
+        environment.API_URL + '/api/examiner/'
+      ).subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
   }
 
   public getMesInscrits(): Observable<Result<Inscription>> {
-
-    return this.http.get<Result<Inscription>>(environment.API_URL + '/api/inscrits/');
-
+    return this.http.get<Result<Inscription>>(
+      environment.API_URL + '/api/inscrits/'
+    );
   }
+  // public getMesInscrits(){
+  //   return new Promise((resolve, reject) => {
+  //     this.http.get(
+  //       environment.API_URL + '/api/inscrits/'
+  //     ).subscribe({
+  //         next: (data) => {
+  //           resolve(data);
+  //         },
+  //         error: (err) => {
+  //           reject(err);
+  //         },
+  //       });
+  //   });
+  // }
 }
