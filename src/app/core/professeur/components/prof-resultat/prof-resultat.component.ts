7db7c6a31d82074bd76d51f435e0b1e37c2eaa66
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-13 23:14:17 +0100

    add pagination file ts to prof resultats

--- src/app/core/professeur/components/prof-resultat/prof-resultat.component.ts
+++ src/app/core/professeur/components/prof-resultat/prof-resultat.component.ts
@@ -11,10 +11,12 @@ import { OperationsService } from '../../services/operations.service';
   styleUrls: ['./prof-resultat.component.css']
 })
 export class ProfResultatComponent implements OnInit {
+  public page: number = 1;
+  itemsCount: number | undefined;
   public alert: AlertData | undefined = undefined;
-  public loading: boolean = false;
+  public loading: boolean = true;
   public resultats: Examiner[] = [];
-  public resultat:Result<Examiner> = {
+  public resultat: Result<Examiner> = {
     count: 0,
     next: '',
     previous: '',
@@ -32,6 +34,29 @@ export class ProfResultatComponent implements OnInit {
   }
   getResultats() {
     this.operationsService.getResultats().then(data => {
+      this.loading = false;
+      this.alert = {
+        type: 'success',
+        message: 'success',
+      };
+      this.resultat = data as Result<Examiner>;
+      this.itemsCount = this.resultat.count
+      this.resultats = this.resultat.results
+    }).catch((err) => {
+      console.log(err);
+      this.alert = {
+        type: 'error',
+        message: "error lors de l'ajout",
+      };
+    }).finally(() => {
+      setTimeout(() => (this.alert = undefined), 3000);
+    });
+  }
+
+  onIndexChange(offset: number) {
+    if (this.loading) return;
+    this.loading = true;
+    this.operationsService.getResultats(offset).then(data => {
       this.loading = false;
       this.alert = {
         type: 'success',
