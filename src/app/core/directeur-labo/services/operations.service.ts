6df1ffe69f5548dc038e8d5a15121ed205cfe6c4
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-17 12:19:59 +0100

    labo commission services

--- src/app/core/directeur-labo/services/operations.service.ts
+++ src/app/core/directeur-labo/services/operations.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Commission } from 'src/app/models/Commission';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,86 @@ import { Injectable } from '@angular/core';
 export class OperationsService {
 
   constructor(private http: HttpClient) { }
+
+  public getSujetsLabo() {
+    return new Promise((resolve, reject) => {
+      this.http.get(environment.API_URL + '/api/sujetslabo/').subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+
+  public addCommission(comm: object) {
+    return new Promise((resolve, reject) => {
+      this.http.post(environment.API_URL + '/api/commission/', comm).subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+
+  public updateSujet(comm: object, id: number) {
+    return new Promise((resolve, reject) => {
+      this.http
+        .put(environment.API_URL + '/api/commission/' + id + '/', comm)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
+
+  public getCommissions() {
+    return new Promise((resolve, reject) => {
+      this.http.get(environment.API_URL + '/api/commission/').subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+
+  public deleteCommission(c: Commission) {
+    return new Promise((resolve, reject) => {
+      this.http
+        .delete(environment.API_URL + '/api/commission/' + c.id + '/')
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
+
+  public getProfesseurs() {
+    return new Promise((resolve, reject) => {
+      this.http.get(environment.API_URL + '/api/labo_professeur/').subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
 }
