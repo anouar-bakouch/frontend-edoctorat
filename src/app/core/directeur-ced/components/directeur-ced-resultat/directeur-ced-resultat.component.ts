c5229ddcede963e1c6022711c0fc66e4035b4080
Mohammed Akram Ibriz <92886184+akramIbriz@users.noreply.github.com>
2022-05-24 12:41:17 +0200

    Update directeur-ced-resultat.component.ts

--- src/app/core/directeur-ced/components/directeur-ced-resultat/directeur-ced-resultat.component.ts
+++ src/app/core/directeur-ced/components/directeur-ced-resultat/directeur-ced-resultat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Examiner } from 'src/app/models/Examiner';
+import Result from 'src/app/models/Result';
+import { AlertData } from 'src/app/shared/components/alert/alert.component';
+import { OperationCedService } from '../../services/operation-ced.service';
 
 @Component({
   selector: '[app-directeur-ced-resultat]',
@@ -6,10 +10,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directeur-ced-resultat.component.css']
 })
 export class DirecteurCedResultatComponent implements OnInit {
-
-  constructor() { }
+  public page: number = 1;
+  itemsCount: number | undefined;
+  public alert: AlertData | undefined = undefined;
+  public loading: boolean = true;
+  public resultats: Examiner[] = [];
+  public resultat: Result<Examiner> = {
+    count: 0,
+    next: '',
+    previous: '',
+    results: []
+  }
+  constructor(private operationsService: OperationCedService) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+    this.getResultats()
+  }
+  getResultats() {
+    this.operationsService.getResultats().then(data => {
+      this.loading = false;
+      this.alert = {
+        type: 'success',
+        message: 'success',
+      };
+      this.resultat = data as Result<Examiner>;
+      this.itemsCount = this.resultat.count
+      this.resultats = this.resultat.results
+    }).catch((err) => {
+      console.log(err);
+      this.alert = {
+        type: 'error',
+        message: "error lors de l'ajout",
+      };
+    }).finally(() => {
+      setTimeout(() => (this.alert = undefined), 3000);
+    });
   }
 
+  onIndexChange(offset: number) {
+    if (this.loading) return;
+    this.loading = true;
+    this.operationsService.getResultats(offset).then(data => {
+      this.loading = false;
+      this.alert = {
+        type: 'success',
+        message: 'success',
+      };
+      this.resultat = data as Result<Examiner>;
+      this.resultats = this.resultat.results
+    }).catch((err) => {
+      console.log(err);
+      this.alert = {
+        type: 'error',
+        message: "error lors de l'ajout",
+      };
+    }).finally(() => {
+      setTimeout(() => (this.alert = undefined), 3000);
+    });
+  }
 }
