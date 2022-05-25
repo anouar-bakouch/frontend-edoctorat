1f29f05ac747cc64bab935c13cbdc0d977baff4f
Mohammed Akram Ibriz <92886184+akramIbriz@users.noreply.github.com>
2022-05-25 13:09:54 +0200

    Update directeur-ced-candidat.component.ts

--- src/app/core/directeur-ced/components/directeur-ced-candidat/directeur-ced-candidat.component.ts
+++ src/app/core/directeur-ced/components/directeur-ced-candidat/directeur-ced-candidat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Examiner } from 'src/app/models/Examiner';
+import { AlertData } from 'src/app/shared/components/alert/alert.component';
+import { OperationCedService } from '../../services/operation-ced.service';
 
 @Component({
   selector: '[app-directeur-ced-candidat]',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directeur-ced-candidat.component.css']
 })
 export class DirecteurCedCandidatComponent implements OnInit {
-
-  constructor() { }
+  public alert: AlertData | undefined = undefined;
+  public loading: boolean = false;
+  public page: number = 1;
+  public itemsCount: number | undefined;
+  public candidats: Examiner[] = []
+  constructor(private operationsService: OperationCedService) { }
 
   ngOnInit(): void {
+    this.getCandidats()
   }
 
+  getCandidats() {
+    this.loading = true
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+    this.operationsService.getCandidats().then(data => {
+      this.loading = false
+      console.log(data.results)
+      this.itemsCount = data.count;
+      this.candidats = data.results
+
+    }).catch((error) => {
+      console.log(error)
+      this.alert = {
+        type: 'error',
+        message: 'error',
+      };
+    }).finally(() => {
+      this.loading = false
+      this.alert = {
+        type: 'success',
+        message: 'success',
+      };
+      setTimeout(() => (this.alert = undefined), 3000);
+    });
+  }
+  onIndexChange(offset: number) {
+    if (this.loading) return;
+    this.loading = true;
+    this.operationsService
+      .getCandidats(offset)
+      .then((d) => {
+        this.candidats = d.results;
+      })
+      .finally(() => (this.loading = false));
+  }
 }
