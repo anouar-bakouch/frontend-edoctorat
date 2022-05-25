fcc11ace5da70d2cf9fc39d86ac2b48179535c9c
anouar <anouar.bakouch@usmba.ac.ma>
2022-05-25 13:12:12 +0100

    fixing the notification model again

--- src/app/core/candidat/components/notifications/notifications.component.ts
+++ src/app/core/candidat/components/notifications/notifications.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import Result from 'src/app/models/Result';
+import { CandidatNotificationsService } from '../../services/candidat-notifications.service';
+
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
+
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  public notifications: Notification [] = [];
+
+  constructor(private candidatNotifications : CandidatNotificationsService) { }
 
   ngOnInit(): void {
+      this.getNotifications();
+  }
+
+  getNotifications(){
+    this.candidatNotifications.getNotifications()
+    .then(res=>{
+      console.log(res);
+    })
   }
 
 }
