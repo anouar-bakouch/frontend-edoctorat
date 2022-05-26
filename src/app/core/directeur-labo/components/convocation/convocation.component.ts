027b0d462caa762d168966ad27feecafcadf05d9
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-26 09:54:16 +0100

    add convocation component

---
+++ src/app/core/directeur-labo/components/convocation/convocation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convocation',
+  templateUrl: './convocation.component.html',
+  styleUrls: ['./convocation.component.css']
+})
+export class ConvocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
