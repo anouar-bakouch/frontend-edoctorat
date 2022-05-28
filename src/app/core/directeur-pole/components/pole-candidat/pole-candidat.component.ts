699094d6fa3e90ae1d3d330922a6f005ad3b8c58
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-28 15:47:26 +0100

    add pole candidat ts file

--- src/app/core/directeur-pole/components/pole-candidat/pole-candidat.component.ts
+++ src/app/core/directeur-pole/components/pole-candidat/pole-candidat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Postuler } from 'src/app/models/Postuler';
+import { Sujet } from 'src/app/models/Sujet';
+import { AlertData } from 'src/app/shared/components/alert/alert.component';
+import { PoleCandidatsService } from '../../services/pole-candidats.service';
 
 @Component({
   selector: 'app-pole-candidat',
@@ -6,10 +10,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pole-candidat.component.css']
 })
 export class PoleCandidatComponent implements OnInit {
-
-  constructor() { }
+  public alert: AlertData | undefined = undefined;
+  public loading: boolean = false;
+  public page: number = 1;
+  public itemsCount: number | undefined;
+  public candidats: Postuler[] = [];
+  public sujet_: string = '';
+  public formationDoctorale_: string = '';
+  public isFetchingItems = true;
+  public errorText: string = '';
+  public laboratoire_: string = '';
+  constructor(public poleCandidatsService: PoleCandidatsService) { }
 
   ngOnInit(): void {
+    this.getPoleSubjects();
   }
+  public getPoleSubjects() {
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+    this.poleCandidatsService.fetchPoleCandidats()
+      .then(x => {
+        this.isFetchingItems = false;
+        this.candidats = x.results;
+        this.itemsCount = x.count;
+        this.alert = {
+          type: 'success',
+          message: 'Bienvenue',
+        };
+      }
+      )
+      .catch(error => {
+        this.alert = {
+          type: 'error',
+          message: 'error',
+        };
+      }).finally(() => {
+        setTimeout(() => (this.alert = undefined), 3000);
+      });
+  }
+
+
+  onIndexChange(offset: number) {
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+    this.poleCandidatsService
+      .fetchPoleCandidats(offset)
+      .then((d) => {
+        this.candidats = d.results;
+        this.alert = {
+          type: 'success',
+          message: 'success',
+        };
+      }).catch(error => {
+        this.alert = {
+          type: 'error',
+          message: 'error',
+        };
+      }).finally(() => {
+        setTimeout(() => (this.alert = undefined), 3000);
+      });
+  }
+
 
 }
