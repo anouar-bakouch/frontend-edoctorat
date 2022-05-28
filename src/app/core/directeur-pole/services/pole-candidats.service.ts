fd9fa926a19852c00262a127a51ba7f969bbee17
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-28 15:47:42 +0100

    add pole candidat services

--- src/app/core/directeur-pole/services/pole-candidats.service.ts
+++ src/app/core/directeur-pole/services/pole-candidats.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Postuler } from 'src/app/models/Postuler';
+import Result from 'src/app/models/Result';
+import { Sujet } from 'src/app/models/Sujet';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +11,22 @@ import { Injectable } from '@angular/core';
 export class PoleCandidatsService {
 
   constructor(private http: HttpClient) { }
+  fetchPoleCandidats(offset: number | undefined = undefined): Promise<Result<Postuler>> {
+    let url = `${environment.API_URL}/api/get-all-candidats/`;
+    if (offset) {
+      url = `${url}?limit=50&offset=${offset}`;
+    }
+    return new Promise((resolve, reject) => {
+      this.http
+        .get<Result<Postuler>>(url)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (err) => {
+            reject(err);
+          },
+        });
+    });
+  }
 }
