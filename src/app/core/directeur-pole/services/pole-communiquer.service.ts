c3ef67a0da54af67686be9356034647aca003a5f
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-30 20:06:12 +0100

    add publier services to pole communiquer service

--- src/app/core/directeur-pole/services/pole-communiquer.service.ts
+++ src/app/core/directeur-pole/services/pole-communiquer.service.ts
@@ -1,9 +1,56 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Commission } from 'src/app/models/Commission';
+import Result from 'src/app/models/Result';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoleCommuniquerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public publierSujet() {
+
+    let url = `${environment.API_URL}/api/publier-sujets/`;
+    return new Promise((resolve, reject) => {
+      this.http.patch(url, {}).subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+  public publierListeAttente() {
+
+    let url = `${environment.API_URL}/api/publier-liste-attente/`;
+    return new Promise((resolve, reject) => {
+      this.http.post(url, {}).subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+  public publierListePrincipale() {
+
+    let url = `${environment.API_URL}/api/publier-liste-principale/`;
+    return new Promise((resolve, reject) => {
+      this.http.post(url, {}).subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
 }
