85ff6a91f41acfa6769c5db6f4d0b1de3a8beb86
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-30 20:04:05 +0100

    add publier functions to pole communiquer ts file

--- src/app/core/directeur-pole/components/pole-communiquer/pole-communiquer.component.ts
+++ src/app/core/directeur-pole/components/pole-communiquer/pole-communiquer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Commission } from 'src/app/models/Commission';
+import { AlertData } from 'src/app/shared/components/alert/alert.component';
+import { PoleCommuniquerService } from '../../services/pole-communiquer.service';
 
 @Component({
   selector: 'app-pole-communiquer',
@@ -6,10 +10,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pole-communiquer.component.css']
 })
 export class PoleCommuniquerComponent implements OnInit {
-
-  constructor() { }
+  public alert: AlertData | undefined = undefined;
+  constructor(public poleCommuniquerService: PoleCommuniquerService) { }
 
   ngOnInit(): void {
   }
+  publierSujet() {
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
 
+    this.poleCommuniquerService.publierSujet().then(
+      (data) => {
+        this.alert = {
+          type: 'success',
+          message: 'publier avec succès',
+        };
+      }).catch((err) => {
+
+        this.alert = {
+          type: 'error',
+          message: "error",
+        };
+      }).finally(() => {
+        setTimeout(() => (this.alert = undefined), 3000);
+      });
+  }
+
+  publierListePrincipale() {
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+
+    this.poleCommuniquerService.publierListePrincipale().then(
+      (data) => {
+        this.alert = {
+          type: 'success',
+          message: 'publier avec succès',
+        };
+      }).catch((err) => {
+
+        this.alert = {
+          type: 'error',
+          message: "error",
+        };
+      }).finally(() => {
+        setTimeout(() => (this.alert = undefined), 3000);
+      });
+  }
+
+  publierListeAttente() {
+    
+    this.alert = {
+      type: 'loading',
+      message: 'loading',
+    };
+
+    this.poleCommuniquerService.publierListeAttente().then(
+      (data) => {
+        this.alert = {
+          type: 'success',
+          message: 'publier avec succès',
+        };
+      }).catch((err) => {
+
+        this.alert = {
+          type: 'error',
+          message: "error",
+        };
+      }).finally(() => {
+        setTimeout(() => (this.alert = undefined), 3000);
+      });
+  }
 }
