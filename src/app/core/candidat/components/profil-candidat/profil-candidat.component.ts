ba401ae758d52b0d5f82928fe2d038f1a61dc03c
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-05-30 19:58:58 +0100

    add getcandidatProfile to ts file for candidat profile

--- src/app/core/candidat/components/profil-candidat/profil-candidat.component.ts
+++ src/app/core/candidat/components/profil-candidat/profil-candidat.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 import { Candidat } from 'src/app/models/Candidat';
+import { environment } from 'src/environments/environment';
 import { CandidatService } from '../../services/candidat.service';
 
 @Component({
   selector: 'app-profil-candidat',
   templateUrl: './profil-candidat.component.html',
-  styleUrls: ['./profil-candidat.component.css']
+  styleUrls: ['./profil-candidat.component.css'],
 })
 export class ProfilCandidatComponent implements OnInit {
-
-  public candidat !:Candidat;
-  public isFetchingInfo:boolean = true;
-  public errorText:string = '';
-
-  constructor(public candidatService:CandidatService) { }
+  public candidat!: Candidat;
+  public isFetchingInfo: boolean = true;
+  public errorText: string = '';
+  public id: number;
+  public str: string = environment.API_URL
+  constructor(
+    public candidatService: CandidatService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.getInfo();
+    this.route.params.forEach((e) => {
+      this.id = e['id'];
+    });
+    this.getInfo(this.id);
   }
 
-  getInfo(){
-    this.candidatService.getCandidatInfo().then(res=>{
+  getInfo(id:number) {
+    this.candidatService.getCandidatProfile(id).then((res) => {
       this.isFetchingInfo = false;
       this.candidat = res;
-    })
+      console.log(this.candidat)
+      console.log(res)
+    });
   }
-
 }
