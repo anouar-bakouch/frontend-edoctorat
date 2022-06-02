72bdee3a96724ed45eb261e02511d611cffcf2a1
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-06-02 12:47:11 +0100

    add env url to url of download profile

--- src/app/core/candidat/components/profil-candidat/profil-candidat.component.ts
+++ src/app/core/candidat/components/profil-candidat/profil-candidat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
+import { DiplomeType } from 'src/app/enums/DiplomeType';
 import { Candidat } from 'src/app/models/Candidat';
 import { environment } from 'src/environments/environment';
 import { CandidatService } from '../../services/candidat.service';
@@ -32,6 +33,11 @@ export class ProfilCandidatComponent implements OnInit {
     this.candidatService.getCandidatProfile(id).then((res) => {
       this.isFetchingInfo = false;
       this.candidat = res;
+      this.candidat['diplomes'].forEach(d => {
+        d.annexes.forEach(a => {
+          a.pathFile = `${ environment.API_URL }`+a.pathFile
+        });
+      });
       console.log(this.candidat)
       console.log(res)
     });
