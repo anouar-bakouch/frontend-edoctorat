a9b98805e6d3d696af52ef2a8c3a683a8cb25528
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-06-06 12:27:07 +0100

    Reset password

--- src/app/public/services/auth/password-recovery.service.ts
+++ src/app/public/services/auth/password-recovery.service.ts
@@ -4,18 +4,38 @@ import { first } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PasswordRecoveryService {
-
-  constructor(private client: HttpClient) { }
+  constructor(private client: HttpClient) {}
 
   requestPasswordReset(email: string): Promise<Boolean> {
     return new Promise<Boolean>((resolve, _) => {
-      this.client.post(`${environment.API_URL}/api/request-password-reset/`, { email, redirectUrl: `${window.origin}/home/candidat/perform-reset` }).pipe(first()).subscribe({
-        next: (_) => resolve(true),
-        error: (_) => resolve(false)
-      })
-    })
+      this.client
+        .post(`${environment.API_URL}/api/request-password-reset/`, {
+          email,
+          redirectUrl: `${window.origin}/home/candidat/perform-reset`,
+        })
+        .pipe(first())
+        .subscribe({
+          next: (_) => resolve(true),
+          error: (_) => resolve(false),
+        });
+    });
+  }
+
+  resetPassword(password: string, token: string): Promise<Boolean> {
+    return new Promise<Boolean>((resolve, _) => {
+      this.client
+        .patch(`${environment.API_URL}/api/perform-password-reset/`, {
+          password,
+          token,
+        })
+        .pipe(first())
+        .subscribe({
+          next: (_) => resolve(true),
+          error: (_) => resolve(false),
+        });
+    });
   }
 }
