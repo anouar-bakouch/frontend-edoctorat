a593b7cf3800003e202e51e65d8a56f1c112310f
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-06-07 12:56:36 +0100

    add header component to scolarite

---
+++ src/app/core/scolarite/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import UserInfo from 'src/app/models/UserInfo';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+}
