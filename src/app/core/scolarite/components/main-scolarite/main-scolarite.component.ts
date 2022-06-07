9dfca98df7594993374e66eb8f1e5682bd185ccb
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-06-07 12:49:01 +0100

    add scolarite main component

---
+++ src/app/core/scolarite/components/main-scolarite/main-scolarite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-scolarite',
+  templateUrl: './main-scolarite.component.html',
+  styleUrls: ['./main-scolarite.component.css']
+})
+export class MainScolariteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
