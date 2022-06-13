03a03cb8d4d99ad1cd43565181b6b74dd2b1af1d
Alaoddine Yaden <Miko@mikomoko.onmicrosoft.com>
2022-06-13 10:07:13 +0100

    add scolarite auth component

---
+++ src/app/public/components/auth/scolarite-auth/scolarite-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scolarite-auth',
+  templateUrl: './scolarite-auth.component.html',
+  styleUrls: ['./scolarite-auth.component.css']
+})
+export class ScolariteAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
